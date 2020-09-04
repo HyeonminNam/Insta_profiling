706de2977d97cc5fad02124ba2abb2678957be8b
HyeonminNam <goisugn@gmail.com>
2020-09-04 16:06:27 +0900

    문서에서 keyterm 목록 뽑아내는 함수 추가

--- word_AIC.py
+++ word_AIC.py
@@ -1,4 +1,6 @@
 import math
+from nltk.tokenize import RegexpTokenizer
+
 
 class feature_vector():
 
@@ -6,7 +8,7 @@ class feature_vector():
         pass
 
 
-    def AIC(self, max_like, free_param_num):
+    def _AIC(self, max_like, free_param_num):
         return -2 * max_like + 2 * free_param_num
         # log가 들어가야 하는건지 안 들어가야 하는 건지 잘 모르겠음
         # 논문 (1) 공식에는 log가 들어가지만,
@@ -15,10 +17,9 @@ class feature_vector():
 
 
     # t = term, D_p = Document postivie list, D_n = Document negative list
-    def calculation(self, t, D_p, D_n):
-
+    def _calculation(self, t, D_p, D_n):
         # Documents count for calculation of AIC
-        N11_t, N12_t, N21_t, N22_t = 0.001, 0.001, 0.001, 0.001   # t appears in D_p, t does not appear in D_p, t appears in D_n, t does not appear in D_n
+        N11_t, N12_t, N21_t, N22_t = 0.000001, 0.000001, 0.000001, 0.000001   # t appears in D_p, t does not appear in D_p, t appears in D_n, t does not appear in D_n
         for d in D_p:
             if t in d:
                 N11_t += 1
@@ -39,7 +40,7 @@ class feature_vector():
 
         # maximum likelihood 계산
         # 오류 발생: math.log(0)일 경우 계산이 되지 않음. count가 0일 때는 어떻게 처리할지 확인해야 함.
-        # 일단은 모두 초기값을 0.001로 수정해줘서 오류나지 않게 넘어감.
+        # 일단은 모두 초기값을 매우 작은 수로 수정해줘서 오류나지 않게 넘어감. -> 이렇게 하는게 일반적이라고 함.
         mll_dm_t = N11_t * math.log(N11_t) + N12_t*math.log(N12_t) + N21_t*math.log(N21_t) + N22_t*math.log(N22_t) - N*math.log(N)
         mll_im_t = N_p*math.log(N_p) + N_t*math.log(N_t) +  N_n*math.log(N_n) + N_not_t*math.log(N_not_t) + (-2)*N*math.log(N)
         
@@ -47,16 +48,36 @@ class feature_vector():
     
 
     def evaluation(self, term, D_p, D_n):
-        mll_dm_t, mll_im_t, N_all = self.calculation(term, D_p, D_n)
-        AIC_dm = self.AIC(mll_dm_t, 3)
-        AIC_im = self.AIC(mll_im_t, 2)
+        mll_dm_t, mll_im_t, N_all = self._calculation(term, D_p, D_n)
+        AIC_dm = self._AIC(mll_dm_t, 3)
+        AIC_im = self._AIC(mll_im_t, 2)
         if N_all[5]/N_all[3] > N_all[6]/N_all[4]:
             E_t = AIC_im - AIC_dm + 2
         else:
             E_t = AIC_dm - AIC_im - 2
         return E_t
-
     
+
+    # 추후 인스타그램에 맞게 tokenizer 수정 필요함
+    def _tokenizer(self, doc):
+        retokenize = RegexpTokenizer("[\w]+")
+        token_lst = retokenize.tokenize(doc)
+        return token_lst
+
+    def _term_lst(self, D):
+        term_lst = []
+        for doc in D:
+            term_lst += self._tokenizer(doc)
+        term_lst = list(set(term_lst))
+        return term_lst
+
+    def key_terms(self, D_p, D_n, evaluation_threshold = 0):
+        D = D_p + D_n
+        term_lst = self._term_lst(D)
+        keyterms_dic = {t:round(self.evaluation(t, D_p, D_n), 5) for t in term_lst if self.evaluation(t, D_p, D_n) > evaluation_threshold}
+        return keyterms_dic
+        
+
     def get_feature_vec(self, key_terms, D_p, D_n):
         feature_vec = []
         feature_vec_label = []
@@ -85,14 +106,21 @@ class feature_vector():
 
 
 if __name__ == "__main__":
-    D_p = ['서울 너무 좋아 짱짱']
-    D_n = ['부산 너무 좋아 짱짱', '광주 너무 좋아 짱짱', '강릉 너무 좋아 짱짱', '대전 너무 좋아 짱짱']
+    D_p = ['서울 너무 좋아 짱짱', '서울 광진구 너무 좋아 짱짱', '경기도 너무 좋아 짱짱']
+    D_n = ['부산 너무 좋아 짱짱', '광주 너무 좋아 짱짱', '강릉 너무 좋아 짱짱', '대전 너무 좋아 짱짱', '충청도 대전 좋아']
+
     fv = feature_vector()
+
     print(fv.evaluation('서울', D_p, D_n))
+    print(fv.evaluation('광진구', D_p, D_n))
+    print(fv.evaluation('경기도', D_p, D_n))
     print(fv.evaluation('부산', D_p, D_n))
     print(fv.evaluation('광주', D_p, D_n))
     print(fv.evaluation('짱짱', D_p, D_n))
 
-    vec, vec_label = fv.get_feature_vec(['서울', '부산', '광주', '너무'], D_p, D_n)
+    key_terms = fv.key_terms(D_p, D_n, evaluation_threshold=0)
+    print(key_terms)
+
+    vec, vec_label = fv.get_feature_vec(key_terms, D_p, D_n)
     print(vec)
     print(vec_label)
